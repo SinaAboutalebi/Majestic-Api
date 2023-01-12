b8a6622bfb79c19ecf46df47d9e223b8e0aab136
Zer0Power <zeroopoower@gmail.com>
2023-01-12 21:56:34 +0330

    feat: embedSender API / embed body builder

---
+++ router/embed/src/embedBody.js
@@ -0,0 +1,51 @@
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+
+module.exports = async (body) => {
+
+    let embed = {
+        timestamp: new Date().toISOString(),
+    };
+
+    //Set Embeds Parameters===================================================//
+    if (body.title) {
+        embed.title = body.title;
+    }
+    if (body.url) {
+        embed.url = body.url;
+    }
+
+    if (body.description) {
+        embed.description = body.description;
+    }
+
+    if (body.thumbnail) {
+        embed.thumbnail = {
+            url: body.thumbnail,
+        };
+    }
+
+    if (body.image) {
+        embed.image = {
+            url: body.image,
+        };
+    }
+
+    if (body.footer) {
+        embed.footer = {
+            text: body.footer,
+        };
+    }
+
+    if (body.footerUrl) {
+        embed.footer.icon_url = body.footerUrl;
+    }
+
+    if (body.color) {
+        embed.color = parseInt(body.color.split("#")[1], 16);
+    }
+
+    return embed;
+
+}
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
