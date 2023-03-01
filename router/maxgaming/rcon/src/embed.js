2bcdd325170b20acb066c66ab687a5a6a20e09fa
root <root@localhost.localdomain>
2023-03-01 02:58:28 -0500

    feat: fix all issues (i cant remember xD)

--- router/maxgaming/rcon/src/embed.js
+++ router/maxgaming/rcon/src/embed.js
@@ -2,6 +2,7 @@
 //Packages
 
 const axios = require('axios');
+const config = require('./config.json');
 
 //---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
 //Error 
@@ -20,9 +21,8 @@ exports.error = async (error, channelid, server) => {
 
     const options = {
         method: 'POST',
-        url: "http://127.0.0.1:64220/embed/send",
+        url: "http://api.0powerdev.ir/embed/send",
         headers: {
-            'authorization': '123456',
             'Content-Type': 'application/json'
         },
         data: data
@@ -49,9 +49,37 @@ exports.response = async (str, channelid, server) => {
 
     const options = {
         method: 'POST',
-        url: "http://127.0.0.1:64220/embed/send",
+        url: "http://api.0powerdev.ir/embed/send",
         headers: {
-            'authorization': '123456',
+            'Content-Type': 'application/json'
+        },
+        data: data
+
+    }
+
+    await axios(options)
+
+}
+
+//---------------------------🤍🍷 'Zer0Power 🍷🤍---------------------------//
+//Logs
+
+exports.logs = async (userid, command, server, str) => {
+
+    let data = JSON.stringify({
+        tkn: process.env.RCON_BOT,
+        channelid: config.logchannelid,
+        title: ":warning: Log :",
+        description: `**:bust_in_silhouette: Admin :** <@${userid}> (${userid})\n\n**:page_with_curl: Command :** ${command}\n\n **:satellite: Server : **  ${server.label} (${server.ip}:${server.port}) \n\n **:gear: Response :** ` + "```" + str.toString() + "```",
+        footer: `${server.ip}:${server.port} - ${server.label}`,
+        role: "none",
+        color: "#8c6de7"
+    })
+
+    const options = {
+        method: 'POST',
+        url: "http://api.0powerdev.ir/embed/send",
+        headers: {
             'Content-Type': 'application/json'
         },
         data: data
--- router/maxgaming/rcon/src/getServer.js
+++ router/maxgaming/rcon/src/getServer.js
@@ -12,51 +12,55 @@ module.exports = (channelid) => {
 
     //Get Server Ip Port Based On Channel ID================================//
     switch (channelid) {
-        case "907904725872156752":
+        case "937410276989501440":
             data.ip = config.aim1.ip
             data.port = config.aim1.port
             data.label = "aim1"
             break;
-        case "947773253412352010":
+        case "937410932198490122":
             data.ip = config.aim2.ip
             data.port = config.aim2.port
             data.label = "aim2"
             break;
-        case "947773296194224158":
+        case "937410979204051014":
             data.ip = config.awp1.ip
             data.port = config.awp1.port
             data.label = "awp1"
             break;
-        case "947773335427751937":
+        case "937411018655682580":
             data.ip = config.pub1.ip
             data.port = config.pub1.port
             data.label = "pub1"
             break;
-        case "947773356650942464":
+        case "937411058715471912":
             data.ip = config.pub2.ip
             data.port = config.pub2.port
             data.label = "pub2"
             break;
-        case "947773375449792572":
+        case "937411074557366322":
             data.ip = config.pub3.ip
             data.port = config.pub3.port
             data.label = "pub3"
             break;
-        case "947773397780283402":
+        case "937411089543593984":
             data.ip = config.pub4.ip
             data.port = config.pub4.port
             data.label = "pub4"
             break;
-        case "947773422006566922":
+        case "937411104567619744":
             data.ip = config.pub5.ip
             data.port = config.pub5.port
             data.label = "pub5"
             break;
-        case "1056654594291273778":
+        case "1026977972491657266":
             data.ip = config.pub6.ip
             data.port = config.pub6.port
             data.label = "pub6"
             break;
+        case "937411212151513098":
+            data.ip = config.admin.ip
+            data.port = config.admin.port
+            data.label = "admin mix"
 
         default:
             data = { error: "could'nt find any server by this channelid" } //Return Error
