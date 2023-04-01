df4de2368ce820b16cddc42f4b84f4d0e807b8ed
Zer0Power <zeroopoower@gmail.com>
2023-04-01 10:39:14 +0430

    feat: change res to result

--- router/maxgaming/steamid/routers/profileRouter.js
+++ router/maxgaming/steamid/routers/profileRouter.js
@@ -21,12 +21,14 @@ router.get("/", async (req, res) => {
     else {
         if (url) {
             if (url.includes("steamcommunity.com") && url.includes("steamcommunity.com/id")) { //Basic Url Validator
-                steamidResolver.customUrlToFullInfo(url, async function (err, res) {
+                steamidResolver.customUrlToFullInfo(url, async function (err, result) {
 
                     if (err) return res.status(500).json({ status: 500, error: "Internal Server Error", message: "a generic error occurred on the server" });
-                    if (!res) return res.status(404).json({ status: 404, error: "Not Found", message: "the requested profile not found" });
+                    if (!result) return res.status(404).json({ status: 404, error: "Not Found", message: "the requested profile not found" });
 
-                    let sid = new SteamIDpack(res.steamID64.toString()); //SteamID Pack
+                    let sid = new SteamIDpack(result.steamID64.toString()); //SteamID Pack
+
+                    return 
 
                 })
             }
