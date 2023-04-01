96e0336fb9c72da743dfc4a530eb2b5dcdef3246
Zer0Power <zeroopoower@gmail.com>
2023-04-01 10:38:35 +0430

    feat: SteaamID Pack

--- router/maxgaming/steamid/routers/profileRouter.js
+++ router/maxgaming/steamid/routers/profileRouter.js
@@ -26,6 +26,8 @@ router.get("/", async (req, res) => {
                     if (err) return res.status(500).json({ status: 500, error: "Internal Server Error", message: "a generic error occurred on the server" });
                     if (!res) return res.status(404).json({ status: 404, error: "Not Found", message: "the requested profile not found" });
 
+                    let sid = new SteamIDpack(res.steamID64.toString()); //SteamID Pack
+
                 })
             }
         }
