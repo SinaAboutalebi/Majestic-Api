6cb175c0a2a579b8285eedc456a0c96522a113c9
Zer0Power <zeroopoower@gmail.com>
2023-04-01 10:20:30 +0430

    feat: basic Url Validator And err handler

--- router/maxgaming/steamid/routers/profileRouter.js
+++ router/maxgaming/steamid/routers/profileRouter.js
@@ -18,5 +18,16 @@ router.get("/", async (req, res) => {
     if (!url && !id) { //Check Query Params
         return res.status(400).json({ status: 400, error: "Bad Request", message: "invalid query" });
     }
-    else { }
+    else {
+        if (url) {
+            if (url.includes("steamcommunity.com") && url.includes("steamcommunity.com/id")) { //Basic Url Validator
+                steamidResolver.customUrlToFullInfo(url, async function (err, res) {
+
+                    if (err) return res.status(500).json({ status: 500, error: "Internal Server Error", message: "a generic error occurred on the server" });
+                    if (!res) return res.status(404).json({ status: 404, error: "Not Found", message: "the requested profile not found" });
+
+                })
+            }
+        }
+    }
 })
