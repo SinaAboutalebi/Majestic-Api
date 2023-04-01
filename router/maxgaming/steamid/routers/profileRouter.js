f93cee5c27b3285c31e7c75f0b768146fc49f5a2
Zer0Power <zeroopoower@gmail.com>
2023-04-01 11:15:06 +0430

    feat: steam Profile By Id

--- router/maxgaming/steamid/routers/profileRouter.js
+++ router/maxgaming/steamid/routers/profileRouter.js
@@ -70,6 +70,30 @@ router.get("/", async (req, res) => {
             }
             else { return res.status(404).json({ status: 404, error: "Not Found", message: "the requested profile not found" }); }
         }
+        if (id) {
+            steamidResolver.steamID64ToFullInfo(url, async function (err, result) { //Get Profile Data By Steam64ID
+
+                if (err) return res.status(500).json({ status: 500, error: "Internal Server Error", message: err });
+                if (!result) return res.status(404).json({ status: 404, error: "Not Found", message: "the requested profile not found" });
+
+                let sid = new SteamIDpack(result.steamID64.toString()); //SteamID Pack
+
+                return res.status(200).json({ //Return Profile Data
+                    status: 200,
+                    nickname: result.steamID.toString(),
+                    avatarUrl: result.avatarFull.toString(),
+                    memberSince: result.memberSince.toString(),
+                    onlineState: result.onlineState.toString(),
+                    privacyState: result.privacyState.toString(),
+                    fullUrl: `https://steamcommunity.com/profiles/${result.steamID64}`,
+                    steam64id: result.steamID64.toString(),
+                    steam2id: sid.getSteam2RenderedID(true),
+                    steam3id: sid.getSteam3RenderedID()
+
+
+                })
+            })
+        }
     }
 })
 
